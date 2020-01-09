4fc0c9764a7182d9175d2eb4b83b84983272fe2a
fabsharp <Omahabitch14>
2020-01-09 14:31:57 +0100

    initial commit

---
+++ dist/_r.js
@@ -0,0 +1,35 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = global || self, global._r = factory());
+}(this, (function () { 'use strict';
+
+  console.log("_r v1.0.0")
+
+  function createDefaultScene(canvas) {
+      var engine = new BABYLON.Engine(canvas);
+      // This creates a basic Babylon Scene object (non-mesh)
+      var scene = new BABYLON.Scene(engine);
+      // This creates and positions a free camera (non-mesh)
+      var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+      // This targets the camera to scene origin
+      camera.setTarget(BABYLON.Vector3.Zero());
+      // This attaches the camera to the canvas
+      camera.attachControl(canvas, true);
+      // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+      var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+      // Default intensity is 1. Let's dim the light a small amount
+      light.intensity = 0.7;
+      // Our built-in 'sphere' shape.
+      var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
+      // Move the sphere upward 1/2 its height
+      sphere.position.y = 1;
+      // Our built-in 'ground' shape.
+      var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+      return scene;
+  }
+
+  return createDefaultScene;
+
+})));
+//# sourceMappingURL=_r.js.map
