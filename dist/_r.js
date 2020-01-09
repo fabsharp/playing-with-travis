e5929ab2cd7f9fa99795e860f9947c96ec03e51f
fabsharp <Omahabitch14>
2020-01-09 15:25:39 +0100

    1.0.8

--- dist/_r.js
+++ dist/_r.js
@@ -4,7 +4,7 @@
   (global = global || self, global._r = factory());
 }(this, (function () { 'use strict';
 
-  console.log("_r v1.0.7")
+  console.log("_r v1.0.8")
 
   function createDefaultScene(canvas) {
       var engine = new BABYLON.Engine(canvas);
